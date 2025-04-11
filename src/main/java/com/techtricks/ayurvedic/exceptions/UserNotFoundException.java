07172e7c86136acd174c0d562628396fbd6e7477
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 14:59:52 +0530

    "add:Added the user not found exceptions handling"

---
+++ src/main/java/com/techtricks/ayurvedic/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.techtricks.ayurvedic.exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
