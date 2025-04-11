d60e23ae398350f8809e6f7a19f040fffd6ccfb1
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:40:55 +0530

    "add: added the order model class"

---
+++ src/main/java/com/techtricks/ayurvedic/dto/OrderDTO.java
@@ -0,0 +1,36 @@
+package com.techtricks.ayurvedic.dto;
+import com.techtricks.ayurvedic.models.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OrderDTO {
+    private Long orderId;
+    private LocalDateTime orderDate;
+    private String status;
+    private double totalAmount;
+    private List<OrderItemDTO> orderItems;
+    private String address;
+
+    public OrderDTO(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate();
+        this.totalAmount = order.getTotalAmount();
+        this.status = String.valueOf(order.getStatus());
+        this.address = order.getAddress().toString(); // Assuming `Address` has a `toString()`
+        this.orderItems = order.getOrderItems().stream()
+                .map(OrderItemDTO::new)
+                .collect(Collectors.toList());
+    }
+}
