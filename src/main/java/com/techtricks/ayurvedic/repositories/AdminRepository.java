cf12e3f8dda1978719909894b99e8797d8e99bcd
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:13:20 +0530

    "add:Added the Admin Repo for database handling"

---
+++ src/main/java/com/techtricks/ayurvedic/repositories/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.techtricks.ayurvedic.repositories;
+
+import com.techtricks.ayurvedic.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin>  findByEmail(String email);
+}
