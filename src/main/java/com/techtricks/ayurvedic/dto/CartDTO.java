744d63fd3163e3631cbfb303cdbd805fe9815196
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:41:44 +0530

    "add: added the CartDto for data transferHandling"

---
+++ src/main/java/com/techtricks/ayurvedic/dto/CartDTO.java
@@ -0,0 +1,18 @@
+package com.techtricks.ayurvedic.dto;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+public class CartDTO {
+
+    private Long cartId;
+    private Double totalPrice;
+    private List<ProductDTO> product = new ArrayList<>();
+}
