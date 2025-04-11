f7a818f075d15a44aa409be7548b01dbfd3beb10
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:43:17 +0530

    "add: added the Cart not Founded Exceptions"

---
+++ src/main/java/com/techtricks/ayurvedic/exceptions/CartNotFoundException.java
@@ -0,0 +1,7 @@
+package com.techtricks.ayurvedic.exceptions;
+
+public class CartNotFoundException extends Exception {
+    public CartNotFoundException(String message) {
+        super(message);
+    }
+}
