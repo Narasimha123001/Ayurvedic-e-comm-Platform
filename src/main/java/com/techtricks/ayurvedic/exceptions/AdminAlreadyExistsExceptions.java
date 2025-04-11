d9dd9d381a6d8e1b0ecdfd66be2989ff17ee0f71
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:20:03 +0530

    "add:Added the Admin already Exist exceptions"

---
+++ src/main/java/com/techtricks/ayurvedic/exceptions/AdminAlreadyExistsExceptions.java
@@ -0,0 +1,6 @@
+package com.techtricks.ayurvedic.exceptions;
+
+public class AdminAlreadyExistsExceptions extends Exception {
+    public AdminAlreadyExistsExceptions(String message) {
+    super();}
+}
