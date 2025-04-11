82dd2f1cd0bb2d4302913152167b0368a1634522
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:14:01 +0530

    "add:Added the Admin service and implementation for curd operations "

---
+++ src/main/java/com/techtricks/ayurvedic/services/AdminService.java
@@ -0,0 +1,16 @@
+package com.techtricks.ayurvedic.services;
+
+
+import com.techtricks.ayurvedic.exceptions.AdminAlreadyExistsExceptions;
+import com.techtricks.ayurvedic.models.Admin;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface AdminService {
+
+    public Optional<Admin> authenticate(String email, String password);
+
+    public Admin saveAdmin(Admin admin) throws AdminAlreadyExistsExceptions;
+}
