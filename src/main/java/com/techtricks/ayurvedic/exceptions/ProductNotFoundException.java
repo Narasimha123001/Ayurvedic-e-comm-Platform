63a78c2d733317d8fe98c8bc8c166c4d8076e759
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-11 16:02:40 +0530

    "add:Added product exception for productNot Found Exceptions "

---
+++ src/main/java/com/techtricks/ayurvedic/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.techtricks.ayurvedic.exceptions;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
