18fe2073202fe340eb09e890ab5b019eb7483ab0
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-14 01:57:20 +0530

    "update: 'added the model Mapper bean class"

--- src/main/java/com/techtricks/ayurvedic/AyurvedicApplication.java
+++ src/main/java/com/techtricks/ayurvedic/AyurvedicApplication.java
@@ -1,8 +1,10 @@
 package com.techtricks.ayurvedic;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AyurvedicApplication {
@@ -13,6 +15,11 @@ public class AyurvedicApplication {
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(AyurvedicApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
