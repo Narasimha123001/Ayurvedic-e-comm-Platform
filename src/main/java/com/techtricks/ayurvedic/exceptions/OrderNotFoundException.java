6647584e2feb9a688bd9ba5420047349db995ebb
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-14 01:53:26 +0530

    "add: added the Order not found exceptions"

---
+++ src/main/java/com/techtricks/ayurvedic/exceptions/OrderNotFoundException.java
@@ -0,0 +1,12 @@
+package com.techtricks.ayurvedic.exceptions;
+
+
+
+public class OrderNotFoundException extends Exception {
+
+
+    public OrderNotFoundException(String message) {
+
+        super(message);
+    }
+}
