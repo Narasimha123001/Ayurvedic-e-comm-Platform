8e4e4329e187f866f1fb4dd87b05c46d2e16ab4f
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-04-14 01:55:09 +0530

    "update: removing the unwanted imports"

--- src/main/java/com/techtricks/ayurvedic/services/ProductServiceImpl.java
+++ src/main/java/com/techtricks/ayurvedic/services/ProductServiceImpl.java
@@ -5,7 +5,6 @@ import com.techtricks.ayurvedic.models.Product;
 import com.techtricks.ayurvedic.repositories.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
